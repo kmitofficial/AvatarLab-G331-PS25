b509429fe7e07bfa62e207f5d4d56d25ab25fffe
nikhileshnilagiri <nikhileshnilagiri20@gmail.com>
2025-04-23 23:02:53 +0530

    protected routes using jwt

--- app/app/(dashboard)/avatars/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function Avatars(){
-
-    return (
-        <div></div>
-    )
-}
--- app/app/api/auth/login/route.ts
+++ app/app/api/auth/login/route.ts
@@ -1,5 +1,7 @@
 import connectMDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import { sign } from "jsonwebtoken";
+import { serialize } from "cookie";
 
 export async function POST(req: NextRequest) {
   try {
@@ -10,13 +12,22 @@ export async function POST(req: NextRequest) {
     const user = await db.collection("users").findOne({ email, password });
 
     if (!user) {
-      return NextResponse.json(
-        { message: "Invalid Credentials" },
-        { status: 404 }
-      );
+      return NextResponse.json( { message: "Invalid Credentials" }, { status: 404 });
     }
 
-    return NextResponse.json({ message: "Successful" }, { status: 200 });
+    const token = sign({ email }, process.env.JWT_SECRET!, {
+      expiresIn: "1h",
+    });
+
+    const serializedCookie = serialize("token", token, {
+      httpOnly: false, secure: process.env.NODE_ENV === "production", sameSite: "strict", path: "/",
+      maxAge: 60 * 60,
+    });
+
+    const response = NextResponse.json({ message: "Successful", email },{ status: 200 });
+    response.headers.set("Set-Cookie", serializedCookie);
+
+    return response;
 
   } catch (error) {
     console.error("Login Error:", error);
